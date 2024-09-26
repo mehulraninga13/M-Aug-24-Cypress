8c8b205d4de914236fc0e8049a68853f9a8a733b
mehulraninga13 <mehul.r@simformsolutions.com>
2024-09-26 16:16:26 +0530

    Report Update

--- cypress.config.js
+++ cypress.config.js
@@ -25,12 +25,24 @@ module.exports = defineConfig({
       // implement node event listeners here
 
        // Delete video for passed tests
-       on('after:spec', (spec, results) => {
-        if (results && results.video && results.stats.failures === 0) {
-          // Remove video if the test passed
-          fs.unlinkSync(results.video);
-        }
-      });
+       on('after:spec', (spec, results) => {  if (results && results.video) 
+        {    // Check if all tests eventually passed    
+          const allEventuallyPassed = results.tests.every((test)=>test.attempts.some((attempt) => attempt.state === 'passed'))
+              if (allEventuallyPassed) 
+                { // Delete the video if all tests eventually passed
+                        fs.unlink(results.video, (err) => 
+                          {        
+                            if (err) 
+                              {         
+                                 console.error('Failed to delete video:', err)        
+                                } else 
+                                {          
+                                  console.log('Deleted video:', results.video)       
+                                 }      
+                                })    
+                              }  
+                            }
+                          })
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
